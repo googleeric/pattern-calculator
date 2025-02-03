6ec1d5036135900d08e6620e10b22fe1e6590745
googleeric <eric.eagle938@gmail.com>
2025-02-04 00:33:05 +0530

    css 110 1day

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def fetch_candlestick(symbol):
 def history():
     try:
         exchange_info = client.get_exchange_info()
-        all_symbols = [s['symbol'] for s in exchange_info['symbols'] if s['symbol'].endswith('USDT')][:150]
+        all_symbols = [s['symbol'] for s in exchange_info['symbols'] if s['symbol'].endswith('USDT')][:110]
 
         all_candlesticks = {}
 
